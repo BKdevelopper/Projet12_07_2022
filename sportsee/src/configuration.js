10053cb21183697c56483e0269ea5c774301519a
bilal kezadri <bilalkezadri@gmail.com>
2022-09-10 16:14:36 +0200

    rappel

--- sportsee/src/configuration.js
+++ sportsee/src/configuration.js
@@ -1,5 +1,5 @@
 const configuration = {
   API: 'http://localhost:3000', // default: 'http://localhost:3000'
-  mockedData: false, // true: data are mocked | false: get data form api
+  mockedData: false, // ( true: data are mocked | false: get data form api ) if you change the param you need refresh page
 }
 export default configuration
